0389b26090406aa985e2cf9fdca5a27e98c52cca
RedCommander735 <40245965+RedCommander735@users.noreply.github.com>
2024-09-14 00:59:51 +0200

    chore: testing stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import { StyleSheet, Platform } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { useMaterialYouTheme } from '@/constants/Theme';
+import { CompoundIcon, Shape } from '@/components/CompoundIcon';
+import { OverviewItem } from '@/components/Overview/OverviewItem';
 
 export default function HomeScreen() {
   return (
@@ -11,25 +13,14 @@ export default function HomeScreen() {
       style={{
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        
       }}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title" style={styles.title}>Übersicht</ThemedText>
       </ThemedView>
 
-      <ThemedView style={{
-        gap: 8,
-        marginBottom: 8,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-        paddingHorizontal: '26%',
-        alignItems: 'center'
-      }}>
-        <MaterialCommunityIcons size={36} name='home' color={useMaterialYouTheme().text} />
-        <ThemedText type="subtitle">8.74 L/km</ThemedText>
-      </ThemedView>
+      <OverviewItem icon={<CompoundIcon icon={<MaterialCommunityIcons name='gas-station' />} secondaryIcon={<MaterialIcons name='route' />} size={36} shape={Shape.Square} />} value={8.746} unit={'L/km'} lastDecimalUp={true} decimals={3}/>
       
     </ThemedView>
   );
